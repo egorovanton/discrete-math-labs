37c467e9ca4bab62851e0735d2cc6160c920b82a
egorov anton <yegorov.ant@gmail.com>
2016-12-09 19:59:35 +0400

    task1 is finished

--- lab2/schedule.hs
+++ lab2/schedule.hs
@@ -1,8 +1,6 @@
 {-# OPTIONS_GHC -O2 #-}
-{-# LANGUAGE BangPatterns #-}
 
 import Data.List hiding (delete)
-import qualified Data.ByteString.Char8 as C
 import System.IO
 import           Data.Set (Set, deleteMax, delete, lookupLE)
 import qualified Data.Set as S
@@ -10,36 +8,25 @@ import Data.Ord
 import Data.Int
 import Data.Maybe (fromJust)
 import Control.Monad
-import Unsafe.Coerce
-
-{-# INLINE parseInt #-}
-parseInt input = do
-    line <- C.hGetLine input 
-    return $! fst $! fromJust $! C.readInt line
-
-{-# INLINE parseInts #-}
-parseInts input = do
-    line <- C.hGetLine input
-    let Just (x, s) = C.readInt line
-        Just (y, _) = C.readInt (C.tail s) 
-    return $! (x, y)
 
 filename = "schedule"
 
+parseInts input = do
+    [x, y] <- map read . words <$> hGetLine input
+    return (x, y)
+
 main = do
     input <- openFile (filename ++ ".in") ReadMode
-    n <- parseInt input
+    n <- read <$> hGetLine input
     list <- forM [1..n] $! \_ -> parseInts input
     writeFile (filename ++ ".out") $! show $! solve (sortBy (comparing $ Down . snd) list) n
-
-
 
 solve :: [(Int, Int)] -> Int -> Int64
 solve list n = helper (S.fromList [1..n]) 0 list
     where
         helper :: Set Int -> Int64 -> [(Int, Int)] -> Int64
-        helper set !acc []                  = acc
-        helper set !acc ((time, fine):rest) = 
+        helper set acc []                  = acc
+        helper set acc ((time, fine):rest) = 
             case lookupLE time set of
                 Nothing  -> helper (deleteMax set) (acc + fromIntegral fine) rest
                 Just tmp -> helper (delete tmp set) acc rest 
